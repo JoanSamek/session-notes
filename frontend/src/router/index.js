1819640f0d5540d8d2cc282e5fdfe8eba77c6be9
Rhegolie <rhegolie@gmail.com>
2022-04-28 13:39:49 +0200

    router update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,13 +6,25 @@ const routes = [
     path: "/",
     name: "main",
     component: MainView,
-    meta: { layout: null },
+    meta: { layout: null, subtitle: "" },
   },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+  scrollBehavior() {
+    return { top: 0 };
+  },
 });
 
+router.beforeEach((to, from, next) => {
+  //check auth, etc.
+  if (true) next();
+});
+
+const defaultTitle = "Lore Hoard";
+router.afterEach((to) => {
+  document.title = defaultTitle + (to.meta.title || "");
+});
 export default router;
